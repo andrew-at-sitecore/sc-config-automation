886bc3538482e3417b93eb170950b2993a57db1b
ac <andrew-at-sitecore@gmail.com>
2016-10-18 12:00:08 -0700

    Implemented routine to strip file names off of "known" extensions

--- sc-config.ps1
+++ sc-config.ps1
@@ -32,6 +32,40 @@ $SCRIPT:CONFIG:DisabledFileExtensions = @('.disabled', '.example')
 $SCRIPT:CONFIG:EnabledFileExtensions = @('.config')
 # END: CONFIGURATION #########################################
 
+# HELPERS ####################################################
+<#
+.Synopsis
+    Strips a config file name from the registered extensions ( to allow matching config files from manifest and those on file system since there can be different combinations of extensions )
+#>
+function Get-BaseConfigFileName {
+    param (
+        [string]$ConfigFileName
+    )
+    $fileNameElements = $ConfigFileName.Trim().Split('.')
+    
+    # process the collection from the tail
+    $cutoffElementIndex = 0
+    for ( $i=-1; $i -gt ($fileNameElements.Length * -1); $i-- ) {
+        # each iteration tries to match element as an extension
+        $extensionMatched = $false
+        $currentIterationFileNameSegment = ".{0}" -f $fileNameElements[$i].ToLower()
+        if ( $SCRIPT:CONFIG:DisabledFileExtensions.Contains($currentIterationFileNameSegment)) { $extensionMatched = $true }
+        if ( $SCRIPT:CONFIG:EnabledFileExtensions.Contains($currentIterationFileNameSegment)) { $extensionMatched = $true }
+
+        if ( -not $extensionMatched ) {
+            #if no extension can be matched from the "tail" - what's left is to be considered the config file base name
+            $cutoffElementIndex = $fileNameElements.Length + $i # $i is negative since the collection had been processed from the "tail"
+            break
+        }
+    }
+
+    return $fileNameElements[0..$cutoffElementIndex] -join '.'
+}
+
+# END: HELPERS ###############################################
+
+
+
 if (-not(Test-Path $Webroot -PathType Container)) {
     throw "'$Webroot' webroot folder not found"
 }
