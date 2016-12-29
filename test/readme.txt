edd47202546b7f222ced21c3e04d83bc6d8757a8
ac <andrew-at-sitecore@gmail.com>
2016-12-29 11:17:40 -0800

    Adding test scripts

---
+++ test/readme.txt
@@ -0,0 +1,3 @@
+This folder contains PowerShell scripts I use to test the cmdlets:
+  - sc-config-automation-workspace  : sets up PowerShell session ( loads module, lists cmdlets )
+  - sc-config-automation-basic-test : runs basic test using the cmdlets
---
+++ test/sc-config-automation-workspace.ps1
@@ -0,0 +1,3 @@
+﻿cd C:\users\ac\Work\Sources\sc-git\sc-config-automation                                                                                                
+import-module -force .\sc-config-src\SC.Config\bin\Debug\SC.Config.dll                                                                                 
+Get-Command -Module sc.config
